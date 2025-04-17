45a454e387086fcfde3274a00336bc34f3612370
GAURAV-KUMAR22 <gorav.panwar99@gmail.com>
2025-04-17 23:18:02 +0530

    add db

--- Server/Db/MongoDb.js
+++ Server/Db/MongoDb.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 const db = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/QrOrder');
+        await mongoose.connect(process.env.DB);
         console.log('Db Connected Successfully')
     } catch (error) {
         console.log(error)
