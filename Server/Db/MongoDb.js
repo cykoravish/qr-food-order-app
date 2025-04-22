e9f09b2a0293868a2893b0407d05c01442c536e6
Gaurav kumar <58880601+GAURAV-KUMAR22@users.noreply.github.com>
2025-04-22 19:27:32 +0530

    Update MongoDb.js

--- Server/Db/MongoDb.js
+++ Server/Db/MongoDb.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const db = async () => {
     try {
-        await mongoose.connect("mongodb+srv://choudharygaurav884:310886Mongodb@cluster0.yo9cuqs.mongodb.net/"); // Only this line is enough in latest versions
+        await mongoose.connect("mongodb+srv://choudharygaurav884:310886Mongodb@cluster0.yo9cuqs.mongodb.net/food-App"); // Only this line is enough in latest versions
         console.log('Db Connected Successfully');
     } catch (error) {
         console.error('Error connecting to MongoDB:', error.message);
