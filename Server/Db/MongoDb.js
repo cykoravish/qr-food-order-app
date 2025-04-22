92b077717842ea63f59b5c793f91fdc76ed83ddc
Gaurav kumar <58880601+GAURAV-KUMAR22@users.noreply.github.com>
2025-04-22 18:32:10 +0530

    Update MongoDb.js

--- Server/Db/MongoDb.js
+++ Server/Db/MongoDb.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 const db = async () => {
     try {
         await mongoose.connect(process.env.DB);
-        console.log('Db Connected Successfully')
+        console.log('Db Connected Successfully',process.env.DB)
     } catch (error) {
         console.log(error)
     }
