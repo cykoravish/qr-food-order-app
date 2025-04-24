f551941c504c67deb84a924339f5fca91092d2af
Gaurav kumar <58880601+GAURAV-KUMAR22@users.noreply.github.com>
2025-04-24 16:16:34 +0530

    Update DashBoardPage.jsx

--- qr-app/src/Pages/Admin/DashBoardPage.jsx
+++ qr-app/src/Pages/Admin/DashBoardPage.jsx
@@ -1,193 +1,129 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import { LoadingCard } from '../../components/LoadingCard';
-import PrivateAxios from '../../Services/PrivateAxios';
-import { CardDetails } from '../../components/CardDetails';
-import publicAxios from '../../Services/PublicAxios';
-import { BsBoxArrowInDownLeft, BsBoxArrowInUp } from "react-icons/bs";
-import { TbCategoryPlus } from "react-icons/tb";
-import { MdAttachMoney } from "react-icons/md";
+import React, { useEffect, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import publicAxios from '../../Services/PublicAxios'
+import { IoIosClose, IoIosDoneAll } from "react-icons/io";
 
-export const DashBoardPage = () => {
-    const [products, setProducts] = useState([]);
-    const [AllOrders, setAllOrders] = useState([]);
-    const [AllSales, setAllSales] = useState([]);
-    const [AllCategory, setAllCategory] = useState([]);
-
-
-    // fetch All Products
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const responce = await PrivateAxios.get('/products');
-                if (responce.status !== 200) {
-                    throw new Error({ message: 'responce failed' })
-                }
-                setProducts(responce.data.data); // Set fetched data
-            } catch (error) {
-                console.error("Error fetching data:", error);
-            }
-        };
-
-        fetchData();
-    }, [])
-
-
-    // Fetch All orders
+export const OrderUpdate = () => {
+    const [orders, setOrders] = useState([]);
+    const [someChanges, setSomeChanges] = useState(false);
+    const navigate = useNavigate();
     useEffect(() => {
         const controller = new AbortController();
-        const fetchedOrder = async () => {
-            const responce = await publicAxios.get('/orders/orders', { signal: controller.signal });
-            if (responce.status !== 200) {
-                throw new Error({ message: 'responce failed' })
+        const fetched = async () => {
+            const res = await publicAxios.get('/orders/orders', { signal: controller.signal });
+            if (res.status !== 200) {
+                throw new Error({ message: 'Responce Failed' })
             };
-            setAllOrders(responce.data.content)
+            setOrders(res.data.content);
         };
-        fetchedOrder();
+        fetched();
+        someChanges && fetched()
         return () => {
-            controller.abort()
+            controller.abort();
         }
-    }, []);
-    console.log(AllOrders);
+    }, [navigate, orders.length, someChanges]);
+    console.log()
 
-    // Fetch sale
-    useEffect(() => {
-        const controller = new AbortController();
-        const fetchedOrder = async () => {
-            const responce = await publicAxios.get('/sales', { signal: controller.signal });
-            if (responce.status !== 200) {
-                throw new Error({ message: 'responce failed' })
-            };
-            setAllSales(responce.data.content)
+    const filterdPendingOrders = orders.filter((item) => item.status === 'pending' || item.status === 'processing');
+    console.log(filterdPendingOrders)
+
+    async function handleDelevery(orderId) {
+        setSomeChanges(true)
+        const responce = await publicAxios.patch(`/orders/${orderId}`, { status: 'delivered' });
+        if (responce !== 200) {
+            throw new Error({ message: 'Something error try after some time' })
         };
-        fetchedOrder();
-        return () => {
-            controller.abort()
-        }
-    }, []);
-    console.log(AllSales)
-
-    useEffect(() => {
-        const controller = new AbortController();
-        const fetchedOrder = async () => {
-            const responce = await publicAxios.get('/products/category', { signal: controller.signal });
-            if (responce.status !== 200) {
-                throw new Error({ message: 'responce failed' })
-            };
-            setAllCategory(responce.data.content)
+        navigate('/admin/pending-orders')
+        setSomeChanges(false)
+    }
+    async function handleCancel(orderId) {
+        const responce = await publicAxios.patch(`/orders/${orderId}`, { status: 'cancelled' });
+        if (responce !== 200) {
+            throw new Error({ message: 'Something error try after some time' })
         };
-        fetchedOrder();
-        return () => {
-            controller.abort()
-        }
-    }, [])
-
-
-    // Today ORders
-    const today = new Date().toISOString().split('T')[0];
-    const todaysOrders = AllOrders.filter(order => {
-        const orderDate = new Date(order.placedAt).toISOString().split('T')[0];
-        return orderDate === today;
-    });
-
-    // Pending Orders
-    const pendingOrders = AllOrders.filter((order) => order.status === 'pending');
-
-
-    // set deta According category
-    const groupedProducts = products.reduce((acc, product) => {
-        const categoryName = product.categoryId?.name || 'Uncategorized';
-        if (!acc[categoryName]) {
-            acc[categoryName] = [];
-        }
-        acc[categoryName].push(product);
-        return acc;
-    }, {});
-
+        alert('order updated')
+    }
+    async function handleProcessing(orderId) {
+        console.log(orderId)
+        setSomeChanges(true)
+        const responce = await publicAxios.patch(`/orders/${orderId}`, { status: 'processing' });
+        if (responce !== 200) {
+            throw new Error({ message: 'Something error try after some time' })
+        };
+        alert('order updated')
+        setSomeChanges(false)
+    }
     return (
-        <div className='max-w-screen-md w-[375px] h-auto] overflow-x-hidden overflow-y-scroll '>
-            <div>
-                <img src='/assets/image1.jpg' alt='logo' />
+        <div className='w-[375px]'>
+            <div className='flex text-left p-4'>
+                <img src="/assets/back.png" alt="back" className='shadow-sm rounded-full' />
+                <Link to="/admin" className='ml-2 font-semibold'>Admin</Link>
             </div>
-
-            {/* Stats section */}
-            <div className='w-[344px] h-[171px] m-4 overflow-x-hidden overflow-y-hidden '>
-                <div className='flex justify-between mb-4'>
-                    <div className='w-[164px] h-[75px] flex flex-col shadow-md  p-2 '>
-                        <Link>
-                            <div className='flex flex-row gap-24 justify-center text-center items-center text-xl'>{todaysOrders.length}<span><BsBoxArrowInUp size={25} /></span> </div>
-
-                            <p>Orders today</p>
-
-
-                        </Link>
-                    </div>
-                    <div className='w-[164px] h-[75px] flex flex-col shadow-md p-2'>
-                        <Link to={'/admin/pending-orders'}>
-                            <div className='flex flex-row gap-24 justify-center text-center items-center text-xl'>{pendingOrders.length}<span><BsBoxArrowInDownLeft size={25} /></span></div>
-                            <p>Pending Orders</p>
-                        </Link>
-                    </div>
-                </div>
-                <div className='flex justify-between mb-4 '>
-                    <div className='w-[164px] h-[75px] flex flex-col shadow-md p-2 overflow-y-hidden'>
-                        <Link to={'/admin/totelsale'}>
-                            <div className='flex flex-row gap-24 justify-center text-center items-center text-xl'>{AllSales ? AllSales.length : 0}<span><MdAttachMoney size={25} /></span></div>
-                            <p>Total Sale</p>
-                        </Link>
-                    </div>
-                    <div className='w-[164px] h-[75px] flex flex-col shadow-md p-2'>
-                        <Link>
-                            <div className='flex flex-row gap-24 justify-center text-center items-center text-xl'>{AllCategory ? AllCategory.length : 0}<span><TbCategoryPlus size={25} /></span></div>
-                            <p>Total Category</p>
-                        </Link>
-                    </div>
-                </div>
+            {filterdPendingOrders.length > 0 ? (<div className="overflow-x-auto w-full">
+                <table className="min-w-full text-sm text-left border ">
+                    <thead className="bg-gray-100">
+                        <tr>
+                            <th className="px-4 py-2 border">Sr. No.</th>
+                            <th className="px-4 py-2 border">Products</th>
+                            <th className="px-4 py-2 border">Quantity</th>
+                            <th className="px-4 py-2 border">Food Processing Action</th>
+                            <th className="px-4 py-2 border">Status</th>
+                            <th className="px-4 py-2 border">Username</th>
+                            <th className="px-4 py-2 border">Payment Method</th>
+                            <th className="px-4 py-2 border">Order Amount</th>
+                            <th className="px-4 py-2 border">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody className='text-xl font-semibold'>
+                        {filterdPendingOrders.map((order, index) => (
+                            <tr key={order._id} className="border-t m-auto in-hover::bg-gray-500">
+                                <td className="px-4 py-2 border">{index + 1}</td>
+                                <td className="px-4 py-2 border">
+                                    <ul className="list-disc ml-4 space-y-1 max-h-24 overflow-y-auto">
+                                        {order.items?.map((item, idx) => (
+                                            <li key={idx}>{item.productId?.name || 'N/A'}</li>
+                                        ))}
+                                    </ul>
+                                </td>
+                                <td className="px-4 py-2 border">
+                                    <ul className="list-disc ml-4 space-y-1 max-h-24 overflow-y-auto">
+                                        {order.items?.map((item, idx) => (
+                                            <li key={idx}>{item.quantity || 'N/A'}</li>
+                                        ))}
+                                    </ul>
+                                </td>
+                                <td>
+                                    <button
+                                        onClick={() => handleProcessing(order._id)}
+                                        className={`flex justify-center w-24 h-10 m-1 rounded cursor-pointer hover:bg-violet-600
+      ${order.status === 'processing' ? 'bg-green-400' : 'bg-yellow-400'}`}
+                                    >
+                                        {order.status}
+                                    </button>
+                                </td>
+                                < td className="px-4 py-2 border capitalize">
+                                    <select name={order.status} id="">
+                                        <option value="">{order.status}</option>
+                                        <option value="processing">Processing</option>
+                                        <option value="">Ready</option>
+                                    </select>
+                                </td>
+                                <td className="px-4 py-2 border">{order.userId?.name}</td>
+                                <td className="px-4 py-2 border">{order.paymentMethod}</td>
+                                <td className="px-4 py-2 border">₹{order.totalAmount}</td>
+                                <td className="px-4 py-2 border gap-6 flex justify-center items-center mt-auto">
+                                    <button className='w-9 h-9 bg-green-400 rounded-full' onClick={() => handleDelevery(order._id)}><IoIosDoneAll size={35} /></button>
+                                    <button className='w-9 h-9 bg-red-400 rounded-full ' onClick={() => handleCancel(order._id)}><IoIosClose size={35} /></button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
             </div>
-            <Link to={'/admin/data-visualize'}>
-                <h2 className='flex flex-row text-center bg-green-200 h-9 justify-center items-center'>Graphical Persentation</h2>
-            </Link>
-
-            {
-                Object.keys(groupedProducts).map((categoryName) => (
-                    <div key={categoryName} className='category-section mb-6'>
-                        <div className='flex justify-between px-4 py-2'>
-                            <h2 className='text-xl font-semibold'>{categoryName}</h2>
-                            <Link to={`/${categoryName}`} state={{ items: groupedProducts[categoryName] }} className='text-blue-500'>
-                                See More
-                            </Link>
-                        </div>
-
-                        <div className='flex overflow-x-auto h-[200px] space-x-4 px-4'>
-                            {groupedProducts[categoryName].map((product) => (
-                                <div key={product._id} className='min-w-[150px] flex-shrink-0'>
-                                    <CardDetails
-                                        id={product._id}
-                                        category={product.categoryId?.name}
-                                        dishName={product.name}
-                                        price={product.price || 100}
-                                        qty={product.quantity} // Adjusted to use `quantity`
-                                        image={product.imageUrl}
-                                          product={product}
-                                    // onAddToCart={() => addToCarts(product)}
-                                    />
-                                </div>
-                            ))}
-                        </div>
-                    </div>
-                ))
+            ) : (<div className='flex justify-center items-center text-center bg-green-300 mt-12 h-24'>
+                <h1 className='text-2xl font-semibold'>There is not pending orders </h1>
+            </div>)
             }
-
-
-            {/* Admin Actions */}
-            <div className='w-[343px] h-[112px] gap-[16px]    overflow-hidden mb-5 '>
-                <div className='w-full bg-[#F9D718] m-4 h-[48px] text-center p-2'>
-                    <Link to='/admin/createProduct'>Create New Item</Link>
-                </div>
-                <div className='w-full m-4 h-[48px] text-center p-2'>
-                    <Link to='/admin/category'>Create New Category</Link>
-                </div>
-            </div>
         </div >
-    );
-};
+    )
+}
