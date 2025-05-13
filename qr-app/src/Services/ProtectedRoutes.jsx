3aad41a565dcd62b569e246cac82ab28ea0e020f
cykoravish <ravishbisht85@gmail.com>
2025-05-13 17:47:45 +0530

    debugging

--- qr-app/src/Services/ProtectedRoutes.jsx
+++ qr-app/src/Services/ProtectedRoutes.jsx
@@ -2,17 +2,18 @@ import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
 
 function getCookies(name) {
+  console.log("document.cookies: ", document.cookie);
   const value = `${document.cookie}`;
   const token = value.split(`${name}=`);
-  console.log("token: ", token)
+  console.log("token: ", token);
   if (token.length === 2) return token.pop().split(" ").shift();
 }
-console.log("getcookies: ",getCookies("token"));
-console.log("document.cookies: ", document.cookie);
+
 // eslint-disable-next-line react-refresh/only-export-components
 export default getCookies;
 
 export const ProtectedRoutes = () => {
+    console.log("getcookies: ", getCookies("token"));
   const isAuthenticated = getCookies("token");
   return isAuthenticated ? <Outlet /> : <Navigate to="/signup" />;
 };
