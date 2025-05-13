88639f34cff9fae1861f09233fbd6629c62ef2a0
cykoravish <ravishbisht85@gmail.com>
2025-05-13 14:48:36 +0530

    modify code to resolve errors

---
+++ README.md
@@ -0,0 +1,18 @@
+# 🍽️ Food Ordering App
+
+A simple food ordering web app built with the MERN stack.
+
+## 🛠️ Tech Stack
+
+- MongoDB
+- Express.js
+- React.js
+- Node.js
+
+## 📦 Setup
+
+```bash
+git clone repo url
+cd food-ordering-app
+npm install
+npm run dev
--- qr-app/src/Services/ProtectedRoutes.jsx
+++ qr-app/src/Services/ProtectedRoutes.jsx
@@ -4,9 +4,11 @@ import { Navigate, Outlet } from "react-router-dom";
 function getCookies(name) {
   const value = `${document.cookie}`;
   const token = value.split(`${name}=`);
+  console.log("token: ", token)
   if (token.length === 2) return token.pop().split(" ").shift();
 }
-console.log(getCookies("token"));
+console.log("getcookies: ",getCookies("token"));
+console.log("document.cookies: ", document.cookie);
 // eslint-disable-next-line react-refresh/only-export-components
 export default getCookies;
 
